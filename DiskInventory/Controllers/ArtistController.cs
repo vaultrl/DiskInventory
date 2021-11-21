8bbefe165a83fe4c37a4863628c8e1af7109de5c
vaultrl <73655721+vaultrl@users.noreply.github.com>
2021-11-20 17:29:46 -0700

    Add files via upload

--- DiskInventory/Controllers/ArtistController.cs
+++ DiskInventory/Controllers/ArtistController.cs
@@ -19,5 +19,57 @@ namespace DiskInventory.Controllers
             List<Artist> artists = context.Artist.OrderBy(a => a.Description).ToList();
             return View(artists);
         }
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.ArtistTypes = context.ArtistType.OrderBy(t => t.Description).ToList();
+            return View("Edit", new Artist());
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.ArtistTypes = context.ArtistType.OrderBy(t => t.Description).ToList();
+            var artist = context.Artist.Find(id);
+            return View(artist);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                if (artist.ArtistId == 0)
+                {
+                    context.Artist.Add(artist);
+                }
+                else
+                {
+                    context.Artist.Update(artist);
+                }
+                context.SaveChanges();
+                return RedirectToAction("Index", "Artist");
+            }
+            else
+            {
+                ViewBag.Action = (artist.ArtistId == 0) ? "Add" : "Edit";
+                ViewBag.ArtistTypes = context.ArtistType.OrderBy(t => t.Description).ToList();
+                return View(artist);
+            }
+        }
+       [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var artist = context.Artist.Find(id);
+            return View(artist);
+        }
+        [HttpPost]
+        public IActionResult Delete(Artist artist)
+        {
+            context.Artist.Remove(artist);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Artist");
+
+        }
     }
 }
